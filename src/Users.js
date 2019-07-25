9829b39fbbf71a125f44760571dab9026677f229
unknown <muhannadjudah@gmail.com>
2019-07-25 14:33:19 +0200

    add remove function

--- src/Users.js
+++ src/Users.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
-const List = ({ surname, gender, region }) => {
+const List = ({ surname, gender, region, email, age }) => {
   return (
     <ul>
       <li>
         <span>{surname}</span>
       </li>
+      <li>Email: {email}</li>
+      <li>Age: {age}</li>
       <li>Gender: {gender}</li>
       <li>Country of origin: {region}</li>
     </ul>
@@ -20,24 +22,33 @@ const Image = ({ photo }) => {
   );
 };
 
-const User = ({ name, index, displayDetails, user: selectedUser }) => {
+const User = ({ name, displayDetails, user, index, remove }) => {
   return (
     <React.Fragment>
-      <p
+      <div
+        className="names"
         onClick={() => {
-          displayDetails(selectedUser);
+          displayDetails(user, index);
         }}
       >
         {name}
-      </p>
+        <button
+          className="remove"
+          onClick={() => {
+            remove(index);
+          }}
+        >
+          remove
+        </button>
+      </div>
     </React.Fragment>
   );
 };
 
 class Users extends Component {
   state = {
-    data: [],
-    user: [],
+    allUser: [],
+    selectedUser: [],
     error: false,
     loading: false,
     displayBox: false,
@@ -48,18 +59,24 @@ class Users extends Component {
     this.setState({ loading: true });
     fetch(url)
       .then(response => response.json())
-      .then(users => this.setState({ data: users, loading: false }))
+      .then(users => this.setState({ allUser: users, loading: false }))
       .catch(err => {
         this.setState({ error: true, loading: false });
       });
   };
 
-  displayDetails = user => {
-    this.setState({ user, displayBox: true });
+  displayDetails = (user, x) => {
+    this.setState({ selectedUser: user, displayBox: true });
+  };
+
+  remove = index => {
+    let allUser = [...this.state.allUser];
+    allUser.splice(index, 1);
+    this.setState({ allUser });
   };
 
   render() {
-    const { loading, error, data } = this.state;
+    const { loading, error, allUser, selectedUser, displayBox } = this.state;
     if (loading) {
       return <div className="loading">Loading...</div>;
     }
@@ -67,27 +84,35 @@ class Users extends Component {
     if (error) {
       return <div className="error">Opps something went wrong</div>;
     }
+
     return (
       <div className="container">
         <div className="navbar">
           <button onClick={this.getData}>Get Users</button>
         </div>
         <div className="parent">
+          {this.state.allUser.length === 0 ? (
+            <div className="no-data">there is no dara ...</div>
+          ) : (
+            ''
+          )}
           <div>
-            {this.state.displayBox > false ? (
+            {displayBox > false ? (
               <div className="details">
-                <Image photo={this.state.user.photo} />
+                <Image photo={selectedUser.photo} />
                 <List
-                  surname={this.state.user.surname}
-                  region={this.state.user.region}
-                  gender={this.state.user.gender}
+                  surname={selectedUser.surname}
+                  region={selectedUser.region}
+                  gender={selectedUser.gender}
+                  email={selectedUser.email}
+                  age={selectedUser.age}
                 />
               </div>
             ) : null}
           </div>
 
           <div className="users">
-            {data.map((item, index) => {
+            {allUser.map((item, index) => {
               return (
                 <div key={index}>
                   <User
@@ -95,6 +120,7 @@ class Users extends Component {
                     user={item}
                     index={index}
                     displayDetails={this.displayDetails}
+                    remove={this.remove}
                   />
                 </div>
               );
@@ -107,3 +133,61 @@ class Users extends Component {
 }
 
 export default Users;
+
+// class Users extends Component {
+//   state = {
+//     loading: false,
+//     error: false,
+//     users: null,
+//     user: null,
+//   };
+// ​
+//   getUsers = () => {
+//     const URL = 'http://uinames.com/api/?ext&amount=5';
+//     this.setState({ loading: true });
+//     fetch(URL)
+//       .then(res => res.json())
+//       .then(data => this.setState({ users: data, loading: false }))
+//       .catch(err => this.setState({ error: true, loading: false }));
+//   };
+// ​
+//   changeCurrentUser = name => {
+//     const userValue = name;
+//     const { users } = this.state;
+//     const currentUser = users.filter(({ name }) => name === userValue);
+//     this.setState({ user: currentUser });
+//   };
+// ​
+//   render() {
+//     console.log(this.state);
+//     const { users, loading, error, user } = this.state;
+// ​
+//     if (loading) {
+//       return <div>Loading...</div>;
+//     }
+// ​
+//     if (error) {
+//       return <div>Some error here</div>;
+//     }
+// ​
+//     return (
+//       <div>
+//         <div>
+//           <button onClick={() => this.getUsers()}>Get Users!</button>
+//         </div>
+//         <ul className="users-list">
+//           {users ? (
+//             users.map(({ name }, index) => (
+//               <li key={index} className="user" onClick={event => this.changeCurrentUser(name)}>
+//                 {name}
+//               </li>
+//             ))
+//           ) : (
+//             <li>No user to show yet...</li>
+//           )}
+//         </ul>
+//         {user ? <User user={user} /> : null}
+//       </div>
+//     );
+//   }
+// }
